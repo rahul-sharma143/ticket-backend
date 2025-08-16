694ce047180ade6fdbc3dad5839af0a9d42bf521
rahul-sharma143 <denielsam143@gmail.com>
2025-08-16 17:53:09 +0530

    url updated

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "ticket-frontend-lac.vercel.app",
+    credentials: true,
+  })
+);
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
